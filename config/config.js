05f53d83449db1c6a3567b43c2ad1456a3bf687c
med amine <aminelife86@gmail.com>
2022-12-24 00:11:14 +0100

    seconde save config

--- config/config.js
+++ config/config.js
@@ -1,11 +1,12 @@
-
-const config = {
- 
+let config;
+if (process.env.JAWSDB_URL) {
+  config = { JAWSDB_URL: process.env.JAWSDB_URL };
+} else {
+  config = {
     host: process.env.DB_HOST,
     user: process.env.DB_USERNAME,
     database: process.env.DB_DATABASE,
     password: process.env.DB_PASSWORD,
-    
-   
-};
+  };
+}
 module.exports = config;
