97ecae4ee3743a4fc221efac9bad89707e85c66b
med amine <aminelife86@gmail.com>
2022-12-24 01:18:00 +0100

    save treize

--- db/db.js
+++ db/db.js
@@ -2,7 +2,7 @@ const mysql = require("mysql2/promise");
 const config = require("../config/config");
 
 async function query(sql, params) {
-  const connection = await mysql.createConnection();
+  const connection = await mysql.createConnection(config);
   const [results] = await connection.execute(sql, params);
 
   return results;
