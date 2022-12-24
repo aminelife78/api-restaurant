3e830e7f01dcb7a94f14d0632f1276878d419258
med amine <aminelife86@gmail.com>
2022-12-24 21:49:59 +0100

    route and controllers categories

--- controllers/categories.controllers.js
+++ controllers/categories.controllers.js
@@ -27,11 +27,27 @@ const createCategory = asyncHandler(async (req, res) => {
   res.status(201).json({message:"categories bien ajouter"})
 })
 
+// modifier une categorie
+const updateCategory = asyncHandler(async (req, res) => {
+  const {id} = req.params;
+  const {name} = req.body;
+  await db.query("UPDATE categories SET name=? WHERE id=?",[name,id])
+  res.status(200).json({message:`la categorie avec id:${id} est bien modifier`})
+})
+
+// suprimer une categorie
+const deleteCategory = asyncHandler(async (req, res) => {
+  const {id} = req.params;
+  await db.query("DELETE FROM categories WHERE id=?",[id])
+  res.status(200).json({message:`la categorie avec id:${id} est bien supprimer`})
+})
 
 
-// 
+// exporte crud les categories
 module.exports = {
   getCategories,
   getCategory,
-  createCategory
+  createCategory,
+  updateCategory,
+  deleteCategory
 };
--- routes/categories.routes.js
+++ routes/categories.routes.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
-const {getCategories,getCategory,createCategory} = require ("../controllers/categories.controllers.js")
+const {getCategories,getCategory,createCategory,updateCategory,deleteCategory} = require ("../controllers/categories.controllers.js")
 
 
 router.get("/",getCategories);
 router.post("/",createCategory);
 router.get("/:id",getCategory);
+router.put("/:id",updateCategory);
+router.delete("/:id",deleteCategory);
 
 module.exports = router;
