bbca174ec307f25a70bd5994cba4c87ebf9b316c
med amine <aminelife86@gmail.com>
2022-12-24 01:05:55 +0100

    huit save config

--- db/db.js
+++ db/db.js
@@ -2,7 +2,7 @@ const mysql = require("mysql2/promise");
 const config = require("../config/config");
 
 async function query(sql, params) {
-  const connection = await mysql.createConnection(config);
+  const connection = await mysql.createConnection("mysql2://cj3959iptt6ohvgu:x9rrld6uusayh4td@au77784bkjx6ipju.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/ucwqfl6wrm6jmf98");
   const [results] = await connection.execute(sql, params);
 
   return results;
