478624452d81f0bfb977fa2943aae70caa7e681e
med amine <aminelife86@gmail.com>
2022-12-24 01:10:56 +0100

    save dix

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 let config;
 if (process.env.JAWSDB_URL) {
-  config = { JAWSDB_URL:"mysql2://cj3959iptt6ohvgu:x9rrld6uusayh4td@au77784bkjx6ipju.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/ucwqfl6wrm6jmf98" };
+  config = process.env.JAWSDB_URL
 } else {
   config = {
     host: process.env.DB_HOST,
