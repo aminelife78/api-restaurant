2f8501988464acd2587b30b4f5c776c5df34e010
med amine <aminelife86@gmail.com>
2023-02-05 00:50:13 +0100

    test cloudinary multer

--- controllers/plats.controllers.js
+++ controllers/plats.controllers.js
@@ -1,50 +1,73 @@
 const multer  = require('multer')
+const { v4: uuidv4 } = require('uuid');
+const sharp = require('sharp');
+const  uploadSingleImage  = require('./multerUploadImages');
+
 const fs = require('fs'); // Added to create directories
-const cloudinary = require('cloudinary').v2
+// const cloudinary = require('cloudinary').v2
+const uploadImage = require("./uploadImage");
+
 
 const path = require('path');
 const db = require("../db/db");
 const asyncHandler = require("express-async-handler");
 const apiError = require("../utils/apiError");
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    fs.mkdir('./uploads/plats',(err)=>{
-      cb(null, './uploads/plats');
-   });
-  },
-  filename: function (req, file, cb) {
-    const ext = file.mimetype.split('/')[1]
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     fs.mkdir('./uploads/plats',(err)=>{
+//       cb(null, './uploads/plats');
+//    });
+//   },
+//   filename: function (req, file, cb) {
+//     const ext = file.mimetype.split('/')[1]
     
-    const filename = `plat-${Date.now()}-${Math.random() * 1E9}.${ext}`
-    cb(null, filename)
-   req.body.image = filename
+//     const filename = `plat-${Date.now()}-${Math.random() * 1E9}.${ext}`
+//     cb(null, filename)
+//    req.body.image = filename
     
   
-  }
-})
+//   }
+// })
 
-const upload = multer({ storage: storage })
-const platUploadImage = upload.single('image')
-cloudinary.config({
-  cloud_name:process.env.CLOUD_NAME,
-  api_key:process.env.API_KEY,
-  api_secret:process.env.API_SECRET 
-})
+// const upload = multer({ storage: storage })
+// const platUploadImage = upload.single('image')
+// cloudinary.config({
+//   cloud_name:process.env.CLOUD_NAME,
+//   api_key:process.env.API_KEY,
+//   api_secret:process.env.API_SECRET 
+// })
 
+// Upload single image
+const uploadPlatsImage = uploadSingleImage('image');
+// Image processing
+const resizeImage = asyncHandler(async (req, res, next) => {
+  const filename =  `plats-${uuidv4()}-${Date.now()}.jpeg`;
 
+  await sharp(req.file.buffer)
+    .resize(600, 600)
+    .toFormat('jpeg')
+    .jpeg({ quality: 95 })
+    .toFile(`uploads/plats/${filename}`);
+
+  // Save image into our db 
+   req.body.image = "http://localhost:5000/plats/" + filename;
+   console.log(req.hostname)
+
+  next();
+});
 
 // recuperer toutes les plats des plats
 const getPlats = asyncHandler(async (req, res) => {
-  const {nameCategory} = req.query
-  console.log(nameCategory)
+  // const {nameCategory} = req.query
+  // console.log(nameCategory)
   // let plats;
   // if(nameCategory){
   //   plats = await db.query("SELECT plats.id ,titre,descreption,prix,image,name FROM plats RIGHT JOIN categories  ON categories.id = plats.categories_id WHERE name=? ",[nameCategory]);
   // }else if(nameCategory ===""){
   //   plats = await db.query("SELECT plats.id ,titre,descreption,prix,image,name FROM plats RIGHT JOIN categories  ON categories.id = plats.categories_id");
   // }
-  plats = await db.query("SELECT plats.id ,titre,descreption,prix,image,name FROM plats RIGHT JOIN categories  ON categories.id = plats.categories_id");
+  const plats = await db.query("SELECT plats.id ,titre,descreption,prix,image,name FROM plats RIGHT JOIN categories  ON categories.id = plats.categories_id");
 
   const countPlats = plats.length;
   res.status(200).json({ result: countPlats, data: plats });
@@ -64,10 +87,15 @@ const getPlat = asyncHandler(async (req, res, next) => {
 // creer une plat
 const createPlat = asyncHandler(async (req, res) => {
   const { titre, descreption, prix,image, categories_id } = req.body;
-  let images = await cloudinary.uploader.upload(req.file.path)
+  
+  // let images = await uploadImage(req.file.path)
+  console.log(image)
+  
+  // console.log(req.hostname)
+  // let images = await cloudinary.uploader.upload(req.file.path)
     await db.query(
       "INSERT INTO plats (titre,descreption,prix,image,categories_id) VALUES (?,?,?,?,?)",
-      [titre, descreption, prix, images.url, categories_id]
+      [titre, descreption, prix, image, categories_id]
     );
        
       
@@ -113,6 +141,8 @@ module.exports = {
   createPlat,
   updatePlat,
   deleteplat,
-  platUploadImage
+  uploadPlatsImage,
+  resizeImage
+  
   
 };
--- routes/plats.routes.js
+++ routes/plats.routes.js
@@ -2,15 +2,15 @@ const express = require("express");
 const router = express.Router();
 
 
-const {getPlats,getPlat,createPlat,updatePlat,deleteplat,platUploadImage} = require ("../controllers/plats.controllers")
+const {getPlats,getPlat,createPlat,updatePlat,deleteplat,uploadPlatsImage,resizeImage} = require ("../controllers/plats.controllers")
 const {authorisation} = require("../controllers/auth.controllers")
 
 
 
 router.get("/",getPlats);
-router.post("/",platUploadImage,authorisation("admin"),createPlat);
+router.post("/",uploadPlatsImage,resizeImage,authorisation("admin"),createPlat);
 router.get("/:id",getPlat);
-router.put("/:id",platUploadImage,authorisation("admin"),updatePlat);
+router.put("/:id",authorisation("admin"),updatePlat);
 router.delete("/:id",authorisation("admin"),deleteplat);
 
 module.exports = router;
