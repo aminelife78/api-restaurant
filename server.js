dcaf1e09a685e0f4a4ea0a8912ee0bd160771fb6
med amine <aminelife86@gmail.com>
2023-02-09 23:00:53 +0100

    essai deploy3

--- server.js
+++ server.js
@@ -4,12 +4,6 @@ const dotenv = require("dotenv");
 const morgan = require("morgan");
 const cors = require("cors");
 
-app.use(express.static(path.join(__dirname, '../front-ecf-restaurant/restaurant/build')));
-
-
-app.get('/*', function (req, res) {
-   res.sendFile(path.join(__dirname, 'build', 'index.html'));
- });
 
 const app = express();
 let session = require("express-session");
