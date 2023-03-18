7db61d6dd5c16d0e952959462ba4e31ac8607417
med amine <aminelife86@gmail.com>
2023-03-18 15:15:18 +0100

    save db

--- config/config.js
+++ config/config.js
@@ -1,12 +1,21 @@
-let config;
-if (process.env.NODE_ENV=='production') {
-  config = process.env.MYSQL_URL
-} else {
-  config = {
-    host: process.env.DB_HOST,
-    user: process.env.DB_USERNAME,
-    database: process.env.DB_DATABASE,
-    password: process.env.DB_PASSWORD,
-  };
-}
+// let config;
+// if (process.env.NODE_ENV=='production') {
+//   config = process.env.MYSQL_URL
+// } else {
+//   config = {
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USERNAME,
+//     database: process.env.DB_DATABASE,
+//     password: process.env.DB_PASSWORD,
+//   };
+// }
+// module.exports = config;
+
+const config = {
+  host: process.env.DB_HOST,
+  user: process.env.DB_USERNAME,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+};
+
 module.exports = config;
