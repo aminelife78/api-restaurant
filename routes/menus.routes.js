5a4a26af7259c2189a627cfc202236ac01d8373c
med amine <aminelife86@gmail.com>
2023-03-18 15:18:51 +0100

    save dbb

--- routes/menus.routes.js
+++ routes/menus.routes.js
@@ -1,14 +1,25 @@
 const express = require("express");
 const router = express.Router();
 
-const {getMenus,getMenu,createMenu,updateMenu,deleteMenu} = require ("../controllers/menus.controllers.js")
-const {authorisation} = require("../controllers/auth.controllers")
-const {getMenuValidator,createMenuValidator,updateMenuValidator,deleteMenuValidator} = require("../utils/validator/menuValidator")
+const {
+  getMenus,
+  getMenu,
+  createMenu,
+  updateMenu,
+  deleteMenu,
+} = require("../controllers/menus.controllers.js");
+const { authorisation } = require("../controllers/auth.controllers");
+const {
+  getMenuValidator,
+  createMenuValidator,
+  updateMenuValidator,
+  deleteMenuValidator,
+} = require("../utils/validator/menuValidator");
 
-router.get("/",getMenus);
-router.post("/",authorisation("admin"),createMenuValidator,createMenu);
-router.get("/:id",getMenuValidator,getMenu);
-router.put("/:id",authorisation("admin"),updateMenuValidator,updateMenu);
-router.delete("/:id",authorisation("admin"),deleteMenuValidator,deleteMenu);
+router.get("/", getMenus);
+router.post("/", createMenuValidator, createMenu);
+router.get("/:id", getMenuValidator, getMenu);
+router.put("/:id", authorisation("admin"), updateMenuValidator, updateMenu);
+router.delete("/:id", authorisation("admin"), deleteMenuValidator, deleteMenu);
 
 module.exports = router;
