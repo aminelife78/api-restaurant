45d2bea48a79c21b5211915303e6ccedfba39f51
med amine <aminelife86@gmail.com>
2023-03-20 10:40:17 +0100

    save dbd

--- config/config.js
+++ config/config.js
@@ -1,23 +1,12 @@
-// let config;
-// if (process.env.NODE_ENV == "production") {
-//   config = process.env.JAWSDB_URL;
-// } else {
-//   config = {
-//     host: process.env.DB_HOST,
-//     user: process.env.DB_USERNAME,
-//     database: process.env.DB_DATABASE,
-//     password: process.env.DB_PASSWORD,
-//   };
-// }
-// module.exports = config;
-
-
-  const config = {
+let config;
+if (process.env.NODE_ENV == "production") {
+  config = process.env.MYSQL_URL;
+} else {
+  config = {
     host: process.env.DB_HOST,
     user: process.env.DB_USERNAME,
     database: process.env.DB_DATABASE,
     password: process.env.DB_PASSWORD,
   };
-
+}
 module.exports = config;
-
