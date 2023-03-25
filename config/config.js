e7f251889dfdc049b5fdd335574ad048488e9496
med amine <aminelife86@gmail.com>
2023-03-25 16:03:34 +0100

    save railway

--- config/config.js
+++ config/config.js
@@ -1,22 +1,12 @@
-// let config;
-// if (process.env.NODE_ENV == "production") {
-//   config = process.env.MYSQL_URL;
-// } else {
-//   config = {
-//     host: process.env.DB_HOST,
-//     user: process.env.DB_USERNAME,
-//     database: process.env.DB_DATABASE,
-//     password: process.env.DB_PASSWORD,
-//   };
-// }
-// module.exports = config;
-
-
-  let config = {
+let config;
+if (process.env.NODE_ENV == "production") {
+  config = process.env.MYSQL_URL;
+} else {
+  config = {
     host: process.env.DB_HOST,
     user: process.env.DB_USERNAME,
     database: process.env.DB_DATABASE,
     password: process.env.DB_PASSWORD,
   };
-
+}
 module.exports = config;
